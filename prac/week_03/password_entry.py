9ee636692599918ed3515014fa4eb1a49b459fd9
jacks <jack.struthers@my.jcu.edu.au>
2019-08-20 11:33:13 +1000

    refactor program to use functions

--- prac/week_03/password_entry.py
+++ prac/week_03/password_entry.py
@@ -1,7 +1,17 @@
-MIN_LENGTH = 4
-user_password = input("Please enter your password: ")
-while len(user_password) < MIN_LENGTH:
-    print("Please enter a password that is at least 4 characters")
+def main():
+
+    min_length = 4
+    user_password = get_password(min_length)
+    encrypted_password = len(user_password) * "*"
+    print(encrypted_password)
+
+
+def get_password(min_length):
     user_password = input("Please enter your password: ")
-encrypted_password = len(user_password) * "*"
-print(encrypted_password)
+    while len(user_password) < min_length:
+        print("Please enter a password that is at least 4 characters")
+        user_password = input("Please enter your password: ")
+    return user_password
+
+
+main()
