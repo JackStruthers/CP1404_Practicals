9b3cc1c0b4416b0aa40e2debdadf2f218de48ec5
jacks <jack.struthers@my.jcu.edu.au>
2019-08-23 09:10:36 +1000

    Add a practice of checking if an input is in a list

--- CP1404_Practicals/week_04/list_exercises.py
+++ CP1404_Practicals/week_04/list_exercises.py
@@ -9,3 +9,12 @@ print("The last number is: {}".format(numbers[-1]))
 print("The smallest number is: {}".format(min(numbers)))
 print("The largest number is: {}".format(max(numbers)))
 print("The average of the numbers is: {:.1f}".format((sum(numbers))/LIMIT))
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username_select = input("Please enter your user name")
+
+if username_select in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
