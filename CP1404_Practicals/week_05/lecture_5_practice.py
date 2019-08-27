0b9e7e13879d1e84d60c31868c46f861b270e738
jacks <jack.struthers@my.jcu.edu.au>
2019-08-28 09:17:02 +1000

    Practice of lists and functions

---
+++ CP1404_Practicals/week_05/lecture_5_practice.py
@@ -0,0 +1,19 @@
+def main():
+    names = ["frank", "jack", "yas", "bob"]
+    ages = [15, 8, 20, 9]
+    oldest_person = find_oldest_person(names, ages)
+    print(oldest_person)
+
+
+def find_oldest_person(names, ages):
+    oldest_position = 0
+    for i in range(len(ages)):
+        if ages[i] > ages[i - 1]:
+            oldest_position = i
+
+    oldest_person = names[oldest_position]
+
+    return oldest_person
+
+
+main()
