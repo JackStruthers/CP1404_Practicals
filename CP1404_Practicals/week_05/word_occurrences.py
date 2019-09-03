5ec41336bbaf30df6f33a30aedfec169bfdc4566
jacks <jack.struthers@my.jcu.edu.au>
2019-09-04 07:19:17 +1000

    Add word counter

---
+++ CP1404_Practicals/week_05/word_occurrences.py
@@ -0,0 +1,17 @@
+word_occurrences = {}
+user_text = input("Text: ")
+split_text = user_text.split(" ")
+
+for word in split_text:
+    try:
+        word_occurrences[word] += 1
+    except KeyError:
+        word_occurrences[word] = 1
+
+longest_count = 0
+for word in split_text:
+    if longest_count < len(word):
+        longest_count = len(word)
+
+for word, occurrence in word_occurrences.items():
+    print("{:<{}} : {}".format(word, longest_count, occurrence))
