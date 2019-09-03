dce81e1bd10698dfb9b79f9aa3bb17f4749ba4c8
jacks <jack.struthers@my.jcu.edu.au>
2019-09-04 09:29:18 +1000

    Update to ask if specific place is true

--- CP1404_Practicals/week_06/list_comp_practice.py
+++ CP1404_Practicals/week_06/list_comp_practice.py
@@ -1,3 +1,3 @@
 products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True]]
-on_sale_products = [sale for sale in products if True in sale]
+on_sale_products = [sale for sale in products if sale[-1] is True]
 print(on_sale_products)
