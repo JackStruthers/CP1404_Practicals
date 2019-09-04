ba3d356c5f8ba847a2e823dbf0af44f8d4cb6a69
jacks <jack.struthers@my.jcu.edu.au>
2019-09-04 12:01:40 +1000

    Update name of the list

--- CP1404_Practicals/week_05/word_occurrences.py
+++ CP1404_Practicals/week_05/word_occurrences.py
@@ -1,16 +1,16 @@
 word_occurrences = {}
 user_text = input("Text: ")
-split_text = user_text.split(" ")
-split_text.sort()
+list_of_text = user_text.split(" ")
+list_of_text.sort()
 
-for word in split_text:
+for word in list_of_text:
     try:
         word_occurrences[word] += 1
     except KeyError:
         word_occurrences[word] = 1
 
 longest_count = 0
-for word in split_text:
+for word in list_of_text:
     if longest_count < len(word):
         longest_count = len(word)
 
