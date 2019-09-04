a1774da653596f956f8f225ca47ec73f510a1151
jacks <jack.struthers@my.jcu.edu.au>
2019-09-04 10:02:28 +1000

    Add a refined if statement

--- CP1404_Practicals/week_06/list_comp_practice.py
+++ CP1404_Practicals/week_06/list_comp_practice.py
@@ -1,3 +1,3 @@
 products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True]]
-on_sale_products = [sale for sale in products if sale[-1] is True]
+on_sale_products = [sale for sale in products if sale[-1]]
 print(on_sale_products)
