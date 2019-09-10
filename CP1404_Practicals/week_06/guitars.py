06427539191d646266b5619e58caf1c182621fb3
jacks <jack.struthers@my.jcu.edu.au>
2019-09-11 08:59:06 +1000

    Add more simple ways to achieve the same thing after looking at the solutions

--- CP1404_Practicals/week_06/guitars.py
+++ CP1404_Practicals/week_06/guitars.py
@@ -1,24 +1,34 @@
 from week_06.guitar import Guitar
 
-guitars = []
 
-"""
-name = input("Name: ")
-while name != "":
-    year = int(input("Year: "))
-    cost = float(input("Cost: "))
-    guitars.append(Guitar(name, year, cost))
+def main():
+
+    guitars = []
+
+    """
     name = input("Name: ")
-"""
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        name = input("Name: ")
+    """
 
-guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-for i, guitar in enumerate(guitars, 1):
-    vintage_string = True if guitar.get_age() >= 50 else False
-    if vintage_string:
-        vintage_string = "(Vintage)"
+    if guitars:
+        guitars.sort()
+
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(Vintage)"
+
+            print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i, guitar.name, guitar.year, guitar.cost,
+                                                                      vintage_string))
     else:
-        vintage_string = ""
+        print("No guitars :( Quick, go and buy one!")
 
-    print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i, guitar.name, guitar.year, guitar.cost, vintage_string))
+
+main()
