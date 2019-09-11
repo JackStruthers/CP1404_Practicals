62eec61cb04c1ad406bd09f81e94255870e03157
jacks <jack.struthers@my.jcu.edu.au>
2019-09-11 10:10:47 +1000

    This commit will simplify the list code

--- CP1404_Practicals/week_06/guitars.py
+++ CP1404_Practicals/week_06/guitars.py
@@ -3,19 +3,15 @@ from week_06.guitar import Guitar
 
 def main():
 
-    guitars = []
+    # guitars = []
+    # name = input("Name: ")
+    # while name != "":
+    #     year = int(input("Year: "))
+    #     cost = float(input("Cost: "))
+    #     guitars.append(Guitar(name, year, cost))
+    #     name = input("Name: ")
 
-    """
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: "))
-        guitars.append(Guitar(name, year, cost))
-        name = input("Name: ")
-    """
-
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Line 6 JTV-59", 2010, 1512.9)]
 
     if guitars:
         guitars.sort()
