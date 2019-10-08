2effedc486dd98165a61589bd851a1c53df945db
jacks <jack.struthers@my.jcu.edu.au>
2019-10-09 09:13:25 +1000

    This commit will split the menu options into more broken down functions

--- CP1404_Practicals/week_08/taxt_sim.py
+++ CP1404_Practicals/week_08/taxt_sim.py
@@ -8,30 +8,46 @@ def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
 
     menu = "q)uit, c)hoose taxi, d)rive"
+
+    user_option(bill, current_taxi, menu, taxis)
+
+
+def user_option(bill, current_taxi, menu, taxis):
     print(menu)
     choice = input(">>>").upper()
 
-    user_option(bill, choice, current_taxi, menu, taxis)
-
-
-def user_option(bill, choice, current_taxi, menu, taxis):
     while choice != "Q":
 
         if choice == "C":
-            print_taxis(taxis)
-            current_taxi = int(input("Chose Taxi: "))
-            show_bill_to_date(bill)
+            current_taxi = run_c_option(bill, taxis)
 
         if choice == "D":
-            bill = bill_calculator(bill, current_taxi, taxis)
-            print("Your {} trip cost you ${:.2f}".format(taxis[current_taxi].name, taxis[current_taxi].get_fare()))
-            show_bill_to_date(bill)
+            bill = run_d_choice(bill, current_taxi, taxis)
 
         print(menu)
         choice = input(">>>").upper()
+
+    run_q_choice(bill, taxis)
+
+
+def run_q_choice(bill, taxis):
     print("Total trip cost: ${:.2f}".format(bill))
     print("Taxis are now:")
     print_taxis(taxis)
+
+
+def run_d_choice(bill, current_taxi, taxis):
+    bill = bill_calculator(bill, current_taxi, taxis)
+    print("Your {} trip cost you ${:.2f}".format(taxis[current_taxi].name, taxis[current_taxi].get_fare()))
+    show_bill_to_date(bill)
+    return bill
+
+
+def run_c_option(bill, taxis):
+    print_taxis(taxis)
+    current_taxi = int(input("Chose Taxi: "))
+    show_bill_to_date(bill)
+    return current_taxi
 
 
 def show_bill_to_date(bill):
