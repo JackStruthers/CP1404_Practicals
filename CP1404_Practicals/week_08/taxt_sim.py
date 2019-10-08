efa68f515ff296d78ec5032e4a48b712876e12e1
jacks <jack.struthers@my.jcu.edu.au>
2019-10-08 21:08:28 +1000

    This commit will split main into functions

--- CP1404_Practicals/week_08/taxt_sim.py
+++ CP1404_Practicals/week_08/taxt_sim.py
@@ -6,32 +6,48 @@ def main():
     current_taxi = None
     bill = 0.0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
     menu = "q)uit, c)hoose taxi, d)rive"
     print(menu)
     choice = input(">>>").upper()
 
+    user_option(bill, choice, current_taxi, menu, taxis)
+
+
+def user_option(bill, choice, current_taxi, menu, taxis):
     while choice != "Q":
 
         if choice == "C":
-            for i, line in enumerate(taxis):
-                print("{} - {}".format(i, line))
+            print_taxis(taxis)
             current_taxi = int(input("Chose Taxi: "))
-            print("Bill to date: ${:.2f}".format(bill))
+            show_bill_to_date(bill)
 
         if choice == "D":
-            distance = int(input("Drive how far? "))
-            taxis[current_taxi].drive(distance)
-            bill += taxis[current_taxi].get_fare()
+            bill = bill_calculator(bill, current_taxi, taxis)
             print("Your {} trip cost you ${:.2f}".format(taxis[current_taxi].name, taxis[current_taxi].get_fare()))
-            print("Bill to date: ${:.2f}".format(bill))
+            show_bill_to_date(bill)
 
         print(menu)
         choice = input(">>>").upper()
-
     print("Total trip cost: ${:.2f}".format(bill))
     print("Taxis are now:")
+    print_taxis(taxis)
+
+
+def show_bill_to_date(bill):
+    print("Bill to date: ${:.2f}".format(bill))
+
+
+def print_taxis(taxis):
     for i, line in enumerate(taxis):
         print("{} - {}".format(i, line))
+
+
+def bill_calculator(bill, current_taxi, taxis):
+    distance = int(input("Drive how far? "))
+    taxis[current_taxi].drive(distance)
+    bill += taxis[current_taxi].get_fare()
+    return bill
 
 
 main()
