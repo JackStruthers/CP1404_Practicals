8436419ca68335eea2db2a0d541ec399637ce3d1
jacks <jack.struthers@my.jcu.edu.au>
2019-10-11 08:59:22 +1000

    This commit will add the initiation of the Movie class

--- 2019-2-a2-JackStruthers/movie.py
+++ 2019-2-a2-JackStruthers/movie.py
@@ -6,4 +6,8 @@
 
 class Movie:
     """..."""
-    pass
+    def __init__(self, title="", category="", year=0, is_watched=False):
+        self.title = title
+        self.category = category
+        self.year = year
+        self.is_watched = is_watched
