45f97277ef2e137df83f0740d899c58dbbff6bda
jacks <jack.struthers@my.jcu.edu.au>
2019-10-12 17:05:58 +1000

    This commit will optimise the sort function

--- 2019-2-a2-JackStruthers/moviecollection.py
+++ 2019-2-a2-JackStruthers/moviecollection.py
@@ -12,11 +12,16 @@ class MovieCollection:
     def __init__(self):
         self.movies = []
 
+    def __str__(self):
+        return "{}".format(self.movies)
+
     def load_movies(self, file_name):
         with open(file_name, "r") as in_file:
             for line in in_file:
+                line = line.strip(" ")
                 line = line.strip("\n")
                 line = line.split(",")
+
                 movie = Movie(line[0], int(line[1]), line[2], line[3])
 
                 if movie.is_watched == "u":
@@ -30,13 +35,10 @@ class MovieCollection:
         self.movies.append(movie)
 
     def sort(self, sort_by):
-        if sort_by == "year":
-            return self.movies.sort(key=operator.attrgetter("year", "title"))
-        elif sort_by == "title":
-            return self.movies.sort(key=operator.attrgetter("title"))
-        elif sort_by == "category":
-            return self.movies.sort(key=operator.attrgetter("category", "title"))
-        elif sort_by == "unwatched":
+        if sort_by == "unwatched":
             return self.movies.sort(key=operator.attrgetter("is_watched", "title"))
         elif sort_by == "watched":
             return self.movies.sort(key=operator.attrgetter("is_watched", "title"), reverse=True)
+        else:
+            return self.movies.sort(key=operator.attrgetter(sort_by, "title"))
+
