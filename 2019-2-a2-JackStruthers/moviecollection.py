e0ddf9e81795ee09cdd2015a57fb60aa3253e138
jacks <jack.struthers@my.jcu.edu.au>
2019-10-12 12:17:37 +1000

    This commit will add the load movie function for my class

--- 2019-2-a2-JackStruthers/moviecollection.py
+++ 2019-2-a2-JackStruthers/moviecollection.py
@@ -3,7 +3,25 @@
 
 # TODO: Create your MovieCollection class in this file
 
+from movie import Movie
+import operator
+
 
 class MovieCollection:
     """..."""
-    pass
+    def __init__(self):
+        self.movies = []
+
+    def load_movies(self, file_name):
+        with open(file_name, "r") as in_file:
+            for line in in_file:
+                line = line.strip("\n")
+                line = line.split(",")
+                movie = Movie(line[0], int(line[1]), line[2], line[3])
+
+                if movie.is_watched == "u":
+                    movie.is_not_watched()
+                else:
+                    movie.has_been_watched()
+
+                self.movies.append(movie)
