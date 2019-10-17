e91694418097aa04f89cacf140bdf24a457a78dd
jacks <jack.struthers@my.jcu.edu.au>
2019-10-17 17:58:22 +1000

    This commit will reformat my first assignment to use my classes

--- 2019-2-a2-JackStruthers/a1_classes.py
+++ 2019-2-a2-JackStruthers/a1_classes.py
@@ -1,10 +1,12 @@
 """
-Replace the contents of this module docstring with your own details
 Name: Jack Struthers
-Date started: 25 - 08 - 19
-GitHub URL: https://github.com/cp1404-students/2019-2-a1-JackStruthers.git
+Date: 17/10/2019
+Brief Project Description: An app that lets you mark movies as watched or not and add new movies to the list
+GitHub URL: https://github.com/cp1404-students/2019-2-a2-JackStruthers.git
 """
 import operator
+from movie import Movie
+from moviecollection import MovieCollection
 
 
 # Opens and closes the initial read file and displays the menu
@@ -12,13 +14,13 @@ def main():
     """..."""
     print("Movies To Watch 1.0 - by Jack Struthers")
 
-    list_of_movies = []
-    movie_file = open("movies.csv", "r")
+    movie_details = MovieCollection()
+    movie_details.load_movies("movies.csv")
 
-    print_movie_file(list_of_movies, movie_file)
-    movie_details = split_movie_list(list_of_movies)
+    for movie in movie_details.movies:
+        print(movie)
 
-    print("{} movies loaded".format(len(movie_details)))
+    print("{} movies loaded from file".format(movie_details.number_of_movies()))
 
     menu = "Menu: \n" \
            "L - List Movies \n" \
@@ -41,16 +43,15 @@ def main():
         print(menu)
         menu_option = option_selection()
 
-    write_movies_to_file(movie_details)
-    print("{} movies saved to movies.csv\nHave a nice day :)".format(len(movie_details)))
-    movie_file.close()
+    movie_details.save_movies("movies.csv")
+    print("{} movies saved to movies.csv\nHave a nice day :)".format(movie_details.number_of_movies()))
 
 
 # Prints the initial movie list and appends to a mast list
-def print_movie_file(list_of_movies, movie_file):
-    for line in movie_file:
-        movie_details = str(line).strip("\n")
-        list_of_movies.append(movie_details)
+# def print_movie_file(list_of_movies, movie_file):
+#     for line in movie_file:
+#         movie_details = str(line).strip("\n")
+#         list_of_movies.append(movie_details)
 
 
 # Error checks user menu selection
@@ -63,56 +64,55 @@ def option_selection():
 
 
 # Splits main list into a list of lists
-def split_movie_list(list_of_movies):
-    movie_details = []
-    for line in list_of_movies:
-        line = str(line).split(',')
-        line[1] = int(line[1])
-        movie_details.append(line)
-
-    return movie_details
+# def split_movie_list(list_of_movies):
+#     movie_details = []
+#     for line in list_of_movies:
+#         line = str(line).split(',')
+#         line[1] = int(line[1])
+#         movie_details.append(line)
+#
+#     return movie_details
 
 
 # Sorts movies in order of year
 def sort_list_by_year(movie_details):
-    movie_details.sort(key=operator.itemgetter(1))
+    movie_details.sort("year")
     return movie_details
 
 
 # Prints an ordered version of the movie list
 def print_ordered_movie_list(movie_details):
-    unwatched_movies = 0
-    watched_movies = 0
-    for i in range(len(movie_details)):
-        if movie_details[i][-1] == 'u':
-            unwatched_movies += 1
-            print("{}. * {:<35} - {:>4} ({})".format
-                  (i, str(movie_details[i][0]), str(movie_details[i][1]), str(movie_details[i][2])))
+    unwatched_movies = movie_details.count_unwatched_movies()
+    watched_movies = movie_details.count_watched_movies()
+    for i, movie in enumerate(movie_details.movies):
+        if not movie.is_watched:
+            print("{}. * {:<35} - {:>4} ({})".format(i, movie.title, movie.year, movie.category))
         else:
-            watched_movies += 1
-            print("{}.   {:<35} - {:>4} ({})".format
-                  (i, str(movie_details[i][0]), str(movie_details[i][1]), str(movie_details[i][2])))
+            print("{}.   {:<35} - {:>4} ({})".format(i, movie.title, movie.year, movie.category))
 
     print("{} movies watched, {} movies still to watch".format(watched_movies, unwatched_movies))
 
 
 # Adds new movies to the list of movie details
 def add_new_movie(movie_details):
-    new_movies = []
+    # new_movies = []
     title = check_title_and_genre("Title: ")
+    # new_movies.append(title)
 
-    new_movies.append(title)
     year = check_number_input()
-    new_movies.append(year)
+    # new_movies.append(year)
 
     category = check_title_and_genre("Category: ")
-    new_movies.append(category)
+    # new_movies.append(category)
 
-    new_movies.append("u")
+    # new_movies.append("u")
 
-    movie_details.append(new_movies)
+    new_movies = Movie(title, year, category, False)
 
-    print("{} ({} from {}) added to movie list".format(str(new_movies[0]), str(new_movies[2]), str(new_movies[1])))
+    movie_details.add_movie(new_movies)
+
+    print("{} ({} from {}) added to movie list".format(str(new_movies.title),
+                                                       str(new_movies.category), str(new_movies.year)))
 
 
 # Ensures the title and category for the movie are not blank
@@ -143,21 +143,30 @@ def check_number_input():
 
 # Sets a movie as watched and checks if all the movies have been watched
 def watch_movie(movie_details):
-    watched_counter = 0
-    for i in range(len(movie_details)):
-        if movie_details[i][-1] == "w":
-            watched_counter += 1
+    watched_counter = movie_details.count_watched_movies()
 
-    if watched_counter == len(movie_details):
+    # for i in range(len(movie_details)):
+    #     if movie_details[i][-1] == "w":
+    #         watched_counter += 1
+
+    if watched_counter == movie_details.number_of_movies():
         print("No more movies to watch!")
     else:
         print("Enter the number of a movie to mark as watched")
         movie_option = check_movie_option(movie_details)
-        if movie_details[movie_option][-1] == "w":
-            print("You have already watched {}".format(movie_details[movie_option][0]))
+
+        movie = None
+
+        for i, movie in enumerate(movie_details.movies):
+            movie = movie
+            if i == movie_option:
+                break
+
+        if movie.is_watched:
+            print("You have already watched {}".format(movie.title))
         else:
-            movie_details[movie_option][-1] = "w"
-            print("{} from {} watched".format(movie_details[movie_option][0], movie_details[movie_option][1]))
+            movie.is_watched = True
+            print("{} from {} watched".format(movie.title, movie.year))
 
     return movie_details
 
@@ -165,13 +174,14 @@ def watch_movie(movie_details):
 # Checks that a movie option is valid
 def check_movie_option(movie_details):
     movie_to_watch = 0
+    number_of_movies = movie_details.number_of_movies()
     valid_movie = False
     while not valid_movie:
         try:
             movie_to_watch = int(input(">>> "))
             if movie_to_watch < 0:
                 print("Number must be >= 0")
-            elif movie_to_watch not in range(len(movie_details)):
+            elif movie_to_watch > number_of_movies:
                 print("Invalid movie number")
             else:
                 valid_movie = True
